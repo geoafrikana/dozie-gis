fffea69578d41e7d61f22a74dfb005d859caaa01
geoafrikana <geoafrikana@gmail.com>
2022-12-24 00:05:52 +0100

    added and read data & attributes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import HomeCSS from './Home.css'
+import bg from './assets/home_bg.png'
 
 function Home() {
   return (
-    <header class="hero row">
-  <div class="container">
+    <header className="hero row">
+  <div className="container">
   <h1>Welcome To Dozie-GIS</h1>
     </div>
 </header>
--- src/WebMap.jsx
+++ src/WebMap.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { MapContainer, TileLayer, LayersControl, GeoJSON } from 'react-leaflet'
+import buildings from './assets/data.json'
 
 function WebMap() {
-  return (
-    <div>WebMap</div>
+  const EdoCenter = [6.446499,5.602304]
+  const stringify = (obj)=>{
+    let str = ''
+    for (const key in obj){
+      str += `<em>${key}</em>: ${obj[key]} <br>`
+    }
+    return str
+  }
+  const onEachFeature = (feature, layer) => {
+    
+    const popupContent = `<Popup> 
+    ${stringify(feature.properties)}
+     </Popup>`
+    layer.bindPopup(popupContent)
+  }
+
+    return (
+    <>
+      <MapContainer center={EdoCenter} zoom={13} scrollWheelZoom={true}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <LayersControl position="topright">
+  <LayersControl.Overlay name="Google Satellite">
+  <TileLayer
+    attribution='&copy; Google Satellite'
+    url="http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}"
+  />
+  </LayersControl.Overlay>
+
+  <LayersControl.Overlay name="Data" >
+  <GeoJSON data={buildings} onEachFeature={onEachFeature} />
+  </LayersControl.Overlay>
+  </LayersControl>
+  
+  
+ </MapContainer>
+    </>
   )
 }
 
